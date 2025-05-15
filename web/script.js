cd14cd3368325115a498e05321a1d38a0af65d35
rasajati <88454765+rasajati@users.noreply.github.com>
2025-05-15 17:01:25 +0700

    Create script.js

---
+++ web/script.js
@@ -0,0 +1,7 @@
+function generateHTML() {
+    let content = document.getElementById('content');
+    content.innerHTML = `
+        <h1>Halaman yang Dibuat Secara Dinamis</h1>
+        <p>Elemen ini dihasilkan dari JavaScript yang dihosting di GitHub.</p>
+    `;
+}
