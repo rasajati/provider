4cc7721d2227820b4f988140c0a2a5bede2c9362
rasajati <88454765+rasajati@users.noreply.github.com>
2026-08-13 12:08:03 +0700

    Delete web/script.js

--- web/script.js
+++
@@ -1,7 +0,0 @@
-function generateHTML() {
-    let content = document.getElementById('content');
-    content.innerHTML = `
-        <h1>Halaman yang Dibuat oleh eddy Secara Dinamis</h1>
-        <p>Elemen ini dihasilkan dari JavaScript yang dihosting di GitHub.</p>
-    `;
-}
